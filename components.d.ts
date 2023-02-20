365c2de16ba56407a52d63642f44e9bce0c21e76
crh888 <nice235@163.com>
2023-02-20 23:01:13 +0800

    lf

--- components.d.ts
+++ components.d.ts
@@ -10,5 +10,8 @@ declare module '@vue/runtime-core' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     VanButton: typeof import('vant/es')['Button']
+    VanCellGroup: typeof import('vant/es')['CellGroup']
+    VanField: typeof import('vant/es')['Field']
+    VanForm: typeof import('vant/es')['Form']
   }
 }
